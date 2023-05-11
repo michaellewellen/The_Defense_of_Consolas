e550d0188daa34295f698bb4ae9ab92347a004cf
michael.lewellen <michael.lewellen@snow.edu>
2023-05-11 16:25:20 -0600

    Hopefully demonstratable

--- Program.cs
+++ Program.cs
@@ -11,8 +11,6 @@ Console.WriteLine($"The tablet is {widthTablet} x {lengthTablet}");
 Console.WriteLine($"The area of the tablet = {widthTablet * lengthTablet}");
 Console.WriteLine($"The perimeter of the tablet = {2*widthTablet + 2*lengthTablet}");
 
-
-
 rectangle yard = new rectangle(5,5,"yard");
 rectangle tablet = new rectangle(11,7,"tablet");
 
@@ -22,3 +20,36 @@ yard.DisplayPerimeter();
 tablet.DefineRectangle();
 tablet.DisplayArea();
 tablet.DisplayPerimeter();
+
+public class rectangle
+{
+ 
+    public rectangle(int x, int y, string z)
+    {
+        width = x;
+        length = y;
+        name = z;
+        area = width * length;
+        perimeter = 2 * width + 2 * length;
+    }
+    public double width;
+    public double length;
+    public string name;
+
+    public double area;
+    public double perimeter;
+    public void DisplayArea()
+    {
+        Console.WriteLine($"The area of the {name} = {area}");
+    }
+    public void DisplayPerimeter()
+    {
+        Console.WriteLine($"The perimeter of the {name} = {perimeter}");
+    }
+    public void DefineRectangle()
+    {
+        Console.WriteLine($"The {name} is {width} x {length}");
+    }
+
+
+}
